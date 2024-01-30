c05b0764d93d99338c90a676fcbdb68814df439f
Paul <paguilar4@ewu.edu>
2024-01-29 22:24:33 -0800

    Updated Jester test

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -5,6 +5,48 @@ namespace CanHazFunny.Tests;
 
 public class JesterTests
 {
- 
+    [Fact]
+    public void TellJoke_ReturnsJoke_Success()
+    {
+        // Arrange
+        var jokeServiceMock = new Mock<IJokeService>();
+        var outputInterfaceMock = new Mock<IJokeServiceOutput>();
+
+        //Return two jokes, neither contain Chuck Norris
+        jokeServiceMock.SetupSequence(s => s.GetJoke())
+            .Returns("Why did the chicken cross the road? To get to the other side!")
+            .Returns("This is another joke");
+
+        var jester = new Jester(jokeServiceMock.Object, outputInterfaceMock.Object);
+
+        // Act
+        jester.TellJoke();
+
+        // Assert
+        outputInterfaceMock.Verify(o => o.Write("Why did the chicken cross the road? To get to the other side!"), Times.Once);
+    }
+
+    [Fact]
+    public void TellJoke_DoesntContainChuckNorris_Success()
+    {
+        // Arrange
+        var jokeServiceMock = new Mock<IJokeService>();
+        var outputInterfaceMock = new Mock<IJokeServiceOutput>();
+
+        // Return a joke containing "Chuck Norris" for the first call,
+        // and a regular joke for subsequent calls to GetJoke()
+        jokeServiceMock.SetupSequence(s => s.GetJoke())
+            .Returns("Why did the chicken cross the road? To get to the other side!")
+            .Returns("Chuck Norris can divide by zero.");
+
+        var jester = new Jester(jokeServiceMock.Object, outputInterfaceMock.Object);
+
+        // Act
+        jester.TellJoke();
+
+        // Assert
+        outputInterfaceMock.Verify(o => o.Write("Why did the chicken cross the road? To get to the other side!"), Times.Once);
+    }
+
 
 }
